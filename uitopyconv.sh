db13d0d84d66dd006377a4ea4095e0f76230e2da
Christian <meandchris3@icloud.com>
2025-11-15 23:41:22 +0000

    updated with error handling

--- uitopyconv.sh
+++ uitopyconv.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-pyside6-uic authentication/test_login.ui -o authentication/login_screen.py
-echo "login screen compiled"
-pyside6-uic authentication/test_signup.ui -o authentication/signup_screen.py
-echo "sign up screen compiled"
-pyside6-uic authentication/test_start.ui -o authentication/start_screen.py
-echo "start screen compiled"
+{
+    pyside6-uic authentication/test_login.ui -o authentication/login_screen.py &&
+    echo "login screen compiled"
+
+} || {
+    echo 'login screen compilation failed'
+}
+
+{
+    pyside6-uic authentication/test_signup.ui -o authentication/signup_screen.py &&
+    echo "sign up screen compiled"
+
+} || {
+    echo 'sign up screen compilation failed'
+}
+
+{
+    pyside6-uic authentication/test_start.ui -o authentication/start_screen.py &&
+    echo "start screen compiled"
+
+} || {
+    echo 'start screen compilation failed'
+}
